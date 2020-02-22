73fe5abb1211fb49c05b2f71c27ca6057e789df4
DannyWu2014 <DannyWu201407@gmail.com>
2020-02-22 21:32:12 +0800

    test log

--- src/main/java/ng/demos/controller/HelloController.java
+++ src/main/java/ng/demos/controller/HelloController.java
@@ -1,14 +1,17 @@
 package ng.demos.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 	
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello, Spring.";
 		
+		log.warn("loging ..");
+		return "Hello, Spring.";
 	}
 }
