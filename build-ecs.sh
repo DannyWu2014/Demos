fa14cdaa965f498b38c6e9f870f6811abad99e3a
DannyWu2014 <DannyWu201407@gmail.com>
2020-02-24 02:15:23 +0800

    add docker support

--- build-ecs.sh
+++ build-ecs.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# docker pull openjdk:8-alpine
 export SPRING_PROFILES_ACTIVE=ecs,data
 ./gradlew -b build-ecs.gradle clean bootRun
 
+./gradlew -b build-ecs.gradle clean jibDockerBuild --image myorg/myapp \
+-Djib.from.image=docker://openjdk:8-alpine
