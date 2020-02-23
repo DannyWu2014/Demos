7d787c869e7e07756bd8f54d0b9edb6b4ccf9d87
danny <danny@ali-ecs>
2020-02-24 04:03:08 +0800

    fix mysql connection

--- scripts/dockerize.sh
+++ scripts/dockerize.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # docker pull openjdk:8-alpine
+docker rm -f  demo
 docker rmi myorg/myapp
 ./gradlew -b build-ecs.gradle clean jibDockerBuild --image myorg/myapp \
 -Djib.from.image=docker://openjdk:8-alpine
 
-docker rm -f  demo
 docker run -d --name demo -p 80:8000 -e SPRING_PROFILES_ACTIVE=ecs,data \
 	myorg/myapp
 
